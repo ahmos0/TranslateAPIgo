b6ca91e47ed443536bf96d745d030250d7a0ce2b
ahmos <kamaboko_0228@icloud.com>
2022-11-19 01:23:53 +0900

    DynamoDBでテーブルを作成し、翻訳データを貯めることができるようになりました. コードが汚いので要修正

--- handler.go
+++ handler.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/ahmos0/goLambdaFirst.git/Testgreeting"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/translate"
 )
 
@@ -17,12 +20,25 @@ type Response struct {
 	Body       string `json:"body"`
 }
 
+type Item struct {
+	TimeStamp   string `json:"timestamp" dynamodbav:"timestamp"`
+	InputTextj  string `json:"inputtext" dynamodbav:"InputText"`
+	OutputTextj string `json:"outputtext" dynamodbav:"OutputText"`
+}
+
 func ExucuteFunc(request events.APIGatewayProxyRequest) (Response, error) {
 	Testgreeting.Hello()
 	InputText := request.QueryStringParameters["InputText"]
 	InputLang := "ja"
 	OutputLang := "en"
-	sess := session.Must(session.NewSession())
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	sessi := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	dynamo := dynamodb.New(sessi)
 	trans := translate.New(sess)
 
 	log.Println(InputText + "これです")
@@ -39,6 +55,27 @@ func ExucuteFunc(request events.APIGatewayProxyRequest) (Response, error) {
 	OutputText := *result.TranslatedText
 
 	fmt.Println(OutputText + "わーい")
+
+	t := time.Now()
+	item := Item{
+		TimeStamp:   *aws.String(t.String()),
+		InputTextj:  *aws.String(InputText),
+		OutputTextj: *aws.String(OutputText),
+	}
+	av, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	tableName := "translate-history"
+	input := &dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item:      av,
+	}
+
+	_, err = dynamo.PutItem(input)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
 	return Response{
 		StatusCode: 200,
